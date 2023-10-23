d34f2e57ed2006e549d3e7e18c8d82350d030e2d
wnexous <andreneexus@hotmail.com>
2023-10-23 20:50:26 -0300

    create controller and adapter dir

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// instance express app
+// instance express appw
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    console.log(req);
+app.use((req, res, next) => {
+    console.log(req.method);
+    next();
+});
+app.get("/:id", (req, res) => {
+    console.log(req.params);
+    res.json(req.params);
 });
 app.listen(4000, () => {
-    console.log("backend iniciado com sucesso");
+    console.log("backend iniciado com sucesSOS.");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import express from "express"
 
 
-// instance express app
+// instance express appw
 const app = express()
 
 
+app.use((req, res, next) => {
+    console.log(req.method);
+
+
+    next()
+})
+
 app.get("/:id", (req, res) => {
-    console.log(req);
+    console.log(req.params);
+
+    res.json(req.params)
 })
 
 
 app.listen(4000, () => {
-    console.log("backend iniciado com sucesso");
+    console.log("backend iniciado com sucesSOS.");
 })
